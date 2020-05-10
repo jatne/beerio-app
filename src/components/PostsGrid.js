d61a9665ee6edd7ef5bbac7ff31429d31185ef43
Paweł Malok <pawelmalok@gmail.com>
2020-05-11 00:31:20 +0200

    filter entries

--- src/components/PostsGrid.js
+++ src/components/PostsGrid.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import Post from './Post';
 
-const PostsGrid = ({ beers }) => {
-  const beer = beers.map((val, index) => <Post key={index} beerData={val} />);
+const PostsGrid = ({ beers, filtered }) => {
+  let beersData = [...beers];
+
+  if (filtered) {
+    const key = Object.keys(filtered)[0];
+    const slug = key !== 'beer' ? key + '_slug' : 'name_slug';
+
+    beersData = beersData.filter(el => el[slug] === filtered[key]);
+  }
+
+  const beer = beersData.map((val, index) =>
+    <Post key={index} beerData={val} />
+  );
 
   return (
     <main className="main">
-      <h1>Najnowsze</h1>
+      <h1>{filtered ? Object.keys(filtered)[0] : 'Najnowsze'}</h1>
       <div className="posts-grid">
         {beer}
       </div>
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -5,7 +5,7 @@ import Stats from './sidebar/Stats';
 
 const Sidebar = ({beers}) => {
   const styles = beers.map(val => val.style);
-  const breweries = beers.map(val => val.brewery.display);
+  const breweries = beers.map(val => val.brewery);
 
   return (
     <aside className="sidebar">
